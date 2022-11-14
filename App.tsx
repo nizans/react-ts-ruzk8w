20a32f8a3c6469b1b06849c030ed39f88e929bfe
Nitzan Savion <59604278+nizans@users.noreply.github.com>
2022-11-14 14:01:44 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,27 +7,24 @@ import SVG from './Svg';
 export default function App() {
   return (
     <div>
-      <SVG />
       <Accordion>
         <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
           <Typography>Accordion 1</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <SVG />
-        </AccordionDetails>
+        ONLY WHEN I SEE THIS ONE - I SEE ALL THE OTHERS: <SVG />
       </Accordion>
       <Accordion>
         <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
           <Typography>Accordion 2</Typography>
         </AccordionSummary>
-        <AccordionDetails></AccordionDetails>
+        I SHOULD SEE SVG HERE: <SVG />
       </Accordion>
       <Accordion disabled>
         <AccordionSummary aria-controls="panel3a-content" id="panel3a-header">
           <Typography>Disabled Accordion</Typography>
         </AccordionSummary>
       </Accordion>
-      <SVG />
+      I SHOULD SEE SVG HERE: <SVG />
     </div>
   );
 }
